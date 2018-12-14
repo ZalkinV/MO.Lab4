95ea4822323b3e6b4e3907f344599f5d06ec210d
Zalkin <Zalkin.viktor@mail.ru>
2018-12-14 14:11:22 +0300

    Add getting features by uniqueness method

--- MO.Lab4.py
+++ MO.Lab4.py
@@ -2,9 +2,26 @@ import numpy
 import pandas
 
 def Main():
+	pandas.set_option('display.max_columns', 81) # Для отображения 81 столбца
+
 	dataPath = "../train.csv"
-	data = pandas.read_csv(dataPath, delimiter=',')
-	print(data)
+	dataFrameRaw = pandas.read_csv(dataPath, sep=',', index_col="Id")
+
+	#print(dataFrameRaw.info()) # Получение инфорации о столбцах
+	#print(dataFrameRaw.describe(include="object")) # Получение информации о всех строках с типом object, чтобы понять уникальных значений какого параметра больше, чтобы использовать это параметр как признак
+	#print(dataFrameRaw["LotConfig"].value_counts()) # Получение количества уникальных значений для указанного столбца
+
+	dataFrame = GetFeatures(dataFrameRaw.drop("SalePrice", axis=1), 15)
+	print(dataFrame)
 	pass
+
+def GetFeatures(dataFrame, minUnique):
+	featuresNames = []
+	for columnName in dataFrame:
+		uniqueCount = len(pandas.unique(dataFrame[columnName]))
+		if uniqueCount >= minUnique:
+			featuresNames.append(columnName)
+	
+	return dataFrame[featuresNames]
 
 Main()
