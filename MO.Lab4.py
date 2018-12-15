72e28aba0b3e454f2d3f41817cc2c1a581a7b8b6
Zalkin <Zalkin.viktor@mail.ru>
2018-12-15 11:27:06 +0300

    Fix name misspelling in call method GetDataParts()

--- MO.Lab4.py
+++ MO.Lab4.py
@@ -15,7 +15,7 @@ def Main():
 	feauturesNames = ["Neighborhood", "LotArea", "YearBuilt"]
 	dataFrame = GetFeatures(dataFrameRaw.drop("SalePrice", axis=1), names=feauturesNames)
 	dataFrameShuff = dataFrame.sample(frac=1) # frac - доля от всех набора данных в случайном порядке
-	dataTrain, dataCross, dataTest = GetDataPart(dataFrameShuff, 0.5, 0.2, 0.3)
+	dataTrain, dataCross, dataTest = GetDataParts(dataFrameShuff, 0.5, 0.2, 0.3)
 
 	print(dataFrame)
 	pass
