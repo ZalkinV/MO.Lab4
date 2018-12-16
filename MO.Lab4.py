b16dade2abbbfb14a72ce28512ed05e0194a6152
Zalkin <Zalkin.viktor@mail.ru>
2018-12-16 17:16:32 +0300

    In print_error() add converting(float64->int64) predicted values

--- MO.Lab4.py
+++ MO.Lab4.py
@@ -50,6 +50,7 @@ def calculate_error(pred, actual, type='rmsle'):
 def print_error(predicted, actual):
 	print("RMSLE = {error}".format(error=calculate_error(predicted, actual)))
 	predicted_actual = pandas.DataFrame({"Predic" : predicted[:8], "Actual" : actual.values[:8]})
+	predicted_actual["Predic"] = predicted_actual["Predic"].astype(np.int64)
 	predicted_actual["Differ"] = predicted_actual["Predic"] - predicted_actual["Actual"]
 	print(predicted_actual, end="\n\n\n")
 	pass
